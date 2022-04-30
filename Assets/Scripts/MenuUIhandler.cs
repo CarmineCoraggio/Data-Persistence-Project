031c868a97bc2dfee9ece3016bc42d0155ccbfd3
Carmine Coraggio <impresacoraggio8819@hotmail.com>
2022-04-30 21:13:51 +0200

    Initial commit

---
+++ Assets/Scripts/MenuUIhandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class MenuUIhandler : MonoBehaviour
+{
+
+    [SerializeField] Text PlayerNameInput;
+
+
+    public void startScene()
+    {
+        SceneManager.LoadScene("main");
+
+      
+    }
+
+    public void SetPlayerName()
+    {
+        PlayerDataHandle.Instance.PlayerName = PlayerNameInput.text;
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
